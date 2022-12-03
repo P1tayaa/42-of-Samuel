7c8f54d0eabb1a83d712f05001df01fe4d683e5f
p1tayaa <fruitdudragonct@gmail.com>
2022-12-03 20:13:39 +0100

    get next line v2 un peu mieux

--- get_next_line/v2/get_next_line.c
+++ get_next_line/v2/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: sboulain <sboulain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 14:37:09 by sboulain          #+#    #+#             */
-/*   Updated: 2022/11/15 18:39:34 by sboulain         ###   ########.fr       */
+/*   Updated: 2022/12/03 17:00:47 by sboulain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,51 +14,6 @@
 #include <unistd.h>
 
 #include <stdio.h>
-
-char	*revome_fist_line_in_buffer(char *buffer)
-{
-	size_t	len_line;
-	size_t	len_buffer;
-	char	*new_buffer;
-	
-	len_line = 0;
-	len_buffer = 0;
-	while (buffer[len_line] != '\n')
-		len_line++;
-	while (buffer[len_buffer] != '\0')
-		len_buffer++;
-	new_buffer = (char *)malloc(len_buffer - len_line + 1);
-	len_line++;
-	len_buffer = 0;
-	while (buffer[len_line] != '\0')
-	{
-		new_buffer[len_buffer] = buffer[len_line];
-		len_buffer++;
-		len_line++;
-	}
-	new_buffer[len_buffer] = '\0';
-	if (buffer)
-		free(buffer);
-	return (new_buffer);
-}
-
-int	get_first_line_len(char *buffer, int buffer_read_len)
-{
-	size_t	len_line;
-
-	len_line = 0;
-	while (buffer[len_line] != '\0')
-	{
-		if (buffer[len_line] == '\n')
-			return (len_line);
-		len_line++;
-	}
-	// printf("len_line = {%ld}/n", len_line);
-
-	if (buffer_read_len != 0)
-		return (len_line);
-	return (0);
-}
 
 char	*free_those(char *char_rd, char *char_rding)
 {
@@ -73,6 +28,48 @@ char	*free_those(char *char_rd, char *char_rding)
 		char_rding = NULL;
 	}
 	return (NULL);
+}
+
+char	*revome_fist_line_in_buffer(char *buffer)
+{
+	size_t	len_line;
+	size_t	len_buffer;
+	char	*new_buffer;
+
+	len_line = 0;
+	len_buffer = 0;
+	while (buffer[len_line] != '\n')
+		len_line++;
+	while (buffer[len_buffer] != '\0')
+		len_buffer++;
+	if (!(new_buffer = (char *) malloc(len_buffer - len_line + 1)))
+		return (NULL);
+	len_line++;
+	len_buffer = 0;
+	while (buffer[len_line] != '\0')
+	{
+		new_buffer[len_buffer] = buffer[len_line];
+		len_buffer++;
+		len_line++;
+	}
+	new_buffer[len_buffer] = '\0';
+	if (buffer)
+		free(buffer);
+	return (new_buffer);
+}
+
+int	get_first_line_len(char *buffer)
+{
+	size_t	len_line;
+
+	len_line = 0;
+	while (buffer[len_line] != '\0')
+	{
+		if (buffer[len_line] == '\n')
+			return (len_line);
+		len_line++;
+	}
+	return (0);
 }
 
 char	*strjoin_an_malloc(char *s1, char *s2, int size_of_s1, int size_of_s2)
@@ -119,26 +116,25 @@ char	*get_next_line(int fd)
 		buffer = strjoin_an_malloc(buffer, read_str, 0, 0);
 	while (buffer != NULL)
 	{
-		if (get_first_line_len(buffer, buffer_read_len) != 0)
+		if (get_first_line_len(buffer) != 0)
 		{
-
 			char		*line;
 			int 		i;
 			int 		len_line;
 			i = 0;
-			len_line = get_first_line_len(buffer, buffer_read_len);
-
+			len_line = get_first_line_len(buffer);
+			// printf("%d/n", i);
 			if (!(line = malloc(len_line + 1 * sizeof(char))))
 				return (NULL);
-				// printf("%d/n", i);
 			while (i <= len_line)
 			{
 				line[i] = buffer[i];
 				i++;
 			}
 			line[i] = '\0';
-			buffer = revome_fist_line_in_buffer(buffer);
-		// printf("%s/n", buffer);
+			if (!(buffer = revome_fist_line_in_buffer(buffer)))
+				return (NULL);
+		// printf("{%s}\n", buffer);
 			return (line);
 		}
 		//read line
@@ -147,9 +143,17 @@ char	*get_next_line(int fd)
 		if (buffer_read_len != 0)
 			buffer = strjoin_an_malloc(buffer, read_str, 0, 0);
 		if (buffer_read_len == 0)
+			buffer = free_those(buffer, NULL);
+		if (buffer_read_len < BUFFER_SIZE)
 		{
-			free(buffer);
+			char *str;
+			str = (char *)malloc(1);
+			if (!str)
+				return (NULL);
+			str = strjoin_an_malloc(str, buffer, 0, 0);
+			free (buffer);
 			buffer = NULL;
+			return (str);
 		}
 	}
 	return (NULL);
@@ -172,13 +176,13 @@ int main()
 		printf("%s", str);
 		free(str);
 		str = get_next_line(fd);
-		printf("%s", str);
-		free(str);
-		str = get_next_line(fd);
-		i++;
+		// printf("%s", str);
+		// free(str);
+		// str = get_next_line(fd);
+		// i++;
 	}
 	str = get_next_line(fd);
 	close(fd);
-	// system("leaks a.out");
+	system("leaks a.out");
 	return (0);
 }
