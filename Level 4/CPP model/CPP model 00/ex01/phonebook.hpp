0ad1fc84c5a23f4bd594fb51619f21c899351a01
p1tayaa <samuel@bemuelsglory>
2023-08-19 09:44:17 +0200

    started to do cpp modules

---
+++ Level 4/CPP model/CPP model 00/ex01/phonebook.hpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phonebook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sboulain <sboulain@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/17 12:09:42 by sboulain          #+#    #+#             */
+/*   Updated: 2023/08/17 12:09:42 by sboulain         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+class contact
+{
+	public:
+	
+	private:
+
+}
+
+class phonebook
+{
+	public:
+		phonebook();
+		~phonebook();
+
+		add_contact();
+		search_contact();
+		exit_prgram();
+	private:
+
+
+}
