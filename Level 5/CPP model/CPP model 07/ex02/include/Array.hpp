fa5c934df5eaf17a121d7ac2d730ceb5c91ee5a8
Samuel Boulain <sboulain@f1r6s17.codam.nl>
2024-07-09 15:56:11 +0200

    cpp 0702 base, but it doesn't quite work

--- Level 5/CPP model/CPP model 07/ex02/include/Array.hpp
+++ Level 5/CPP model/CPP model 07/ex02/include/Array.hpp
@@ -1,24 +1,42 @@
-#ifndef ARRAY_HPP
-# define ARRAY_HPP
+#pragma once
 
-template <class T>
+# include "Array.tpp"
+
+// Construction with no parameter: Creates an empty array.
+// • Construction with an unsigned int n as a parameter: Creates an array of n elements
+// initialized by default.
+// Tip: Try to compile int * a = new int(); then display *a.
+// • Construction by copy and assignment operator. In both cases, modifying either the
+// original array or its copy after copying musn’t affect the other array.
+// • You MUST use the operator new[] to allocate memory. Preventive allocation (al-
+// locating memory in advance) is forbidden. Your program must never access non-
+// allocated memory.
+// • Elements can be accessed through the subscript operator: [ ].
+// • When accessing an element with the [ ] operator, if its index is out of bounds, an
+// std::exception is thrown.
+// • A member function size() that returns the number of elements in the array. This
+// member function takes no parameter and musn’t modify the current instance.
+
+class	InvalidIndexException : public std::exception
+{
+public:
+    virtual const char	*what() const throw();
+};
+
+template <typename T>
 class Array
 {
 private:
-    /* data */
+    uint _size;
+    T   *_first_array;
+    void init_array();
+    InvalidIndexException _error;
 public:
-    Array(/* args */);
+    Array();
+    Array(unsigned int size);
+    Array(Array &dup);
+    Array &operator=(const Array& other);
+    T& operator[](unsigned int i) const;
+    unsigned int size(void);
     ~Array();
 };
-
-
-template <class T> Array::Array(/* args */)
-{
-}
-
-Array::~Array()
-{
-}
-
-
-#endif
---
+++ Level 5/CPP model/CPP model 07/ex02/src/main.cpp
@@ -0,0 +1,13 @@
+# include "../include/Array.hpp"
+
+int main(void) {
+    uint size = 6;
+    Array<int> test(6);
+    // for (uint i = 0; i < size; i++) {
+    //     test[i] = i + 1;
+    // }
+    // for (uint i = 0; i < size; i++) {
+    //     std::cout << test[i] << std::endl;
+    // }
+    return 0;
+}
