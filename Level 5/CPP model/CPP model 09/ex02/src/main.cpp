062b74b0ec1bb4cca9650f0608ba6001e533810f
Samuel Boulain <sboulain@f1r4s12.codam.nl>
2024-09-02 14:49:50 +0200

    the ford algorith now work fine

--- Level 5/CPP model/CPP model 09/ex02/src/main.cpp
+++ Level 5/CPP model/CPP model 09/ex02/src/main.cpp
@@ -18,41 +18,31 @@ int binary_search(std::vector<int> &vector, int cur_start, int cur_size, int val
         else
             return cur_index + 1;
     } else if (vector[cur_index] < value) {
-        return binary_search(vector, cur_index, cur_size / 2, value);
+        return binary_search(vector, cur_index, (cur_size + 1) / 2, value);
     } else {
-        return binary_search(vector, cur_start, cur_size / 2, value);
+        return binary_search(vector, cur_start, (cur_size + 1) / 2, value);
     }
 }
 
-void Pmerge(std::vector<int> &list_num, int size) {
-    int i = 0;
-    int extra_num;
-    int temp;
-    std::vector<int> small;
-    std::vector<int> big;
-    if (size % 2 == 1) {
-        extra_num = list_num[0];
-        i++;
-        std::cout << "extra_num is " << extra_num << std::endl;
-    }
+void    pair_up_and_compare(int i, int size, std::vector<int> &small,
+                            std::vector<int> &big, std::vector<int> &list_num) {
+    std::pair<int, int>        pair_cell;
+
     for (;i < size; i += 2) {
-        if (list_num[i] < list_num[i + 1]) {
-            small.push_back(list_num[i]);
-            big.push_back(list_num[i + 1]);
+        pair_cell.first = list_num[i];
+        pair_cell.second = list_num[i + 1]; 
+        if (pair_cell.first < pair_cell.second) {
+            small.push_back(pair_cell.first);
+            big.push_back(pair_cell.second);
         } else {
-            small.push_back(list_num[i + 1]);
-            big.push_back(list_num[i]);
+            small.push_back(pair_cell.second);
+            big.push_back(pair_cell.first);
         }
     }
-    if (big.size() == 2) {
-        if (big[0] > big[1]){
-            temp = big[0];
-            big[0] = big[1];
-            big[1] = temp;
-        }
-    } else if (big.size() > 1) {
-        Pmerge(big, big.size());
-    }
+}
+
+void merge_sort(int size, int extra_num, std::vector<int> &small, std::vector<int> &big) {
+    int temp;
     for (size_t j = 0; j < small.size(); j++) {
         temp = binary_search(big, 0, big.size(), small[j]);
         if (temp >= int(big.size()))
@@ -68,13 +58,37 @@ void Pmerge(std::vector<int> &list_num, int size) {
         else
             big.insert(big.begin() + temp, extra_num);
     }
+}
+
+void Pmerge(std::vector<int> &list_num, int size) {
+    int extra_num = 0;
+    int temp;
+    std::vector<int> small;
+    std::vector<int> big;
+    if (size % 2 == 1) {
+        extra_num = list_num[0];
+        pair_up_and_compare(1, size, small, big, list_num);
+        std::cout << "extra_num is " << extra_num << std::endl;
+    } else
+        pair_up_and_compare(0, size, small, big, list_num);
+    if (big.size() == 2) {
+        if (big[0] > big[1]){
+            std::cout << "big swap hgappen\n";
+            temp = big[0];
+            big[0] = big[1];
+            big[1] = temp;
+        }
+    } else if (big.size() > 1) {
+        Pmerge(big, big.size());
+    }
+    merge_sort(size, extra_num, small, big);
+    
     for (size_t i = 0; i < big.size(); i++)
     {
         std::cout << big[i] << ", ";
     }
-    std::cout << std::endl << std::endl;;
+    std::cout << std::endl << std::endl;
     list_num = big;
-    // std::cin >> temp;
 }
 
 int main(int argc, char **argv) {
